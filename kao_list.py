1ef2d951fc695435055a74b3c62128d7a4fb451d
Chris Loew <cloew123@gmail.com>
2015-10-01 10:19:42 -0600

    Added basic kao list file

---
+++ kao_list.py
@@ -0,0 +1,3 @@
+
+class KaoList:
+    """ ADD CLASS DESCRIPTION """
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(name='kao_list',
       version='0.0.1',
-      description="",
-      author='',
-      author_email='',
-      packages=['kao_list'],
+      description="List Wrapper class that provides extra convenience methods such as first and last",
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      py_modules =['kao_list'],
      )
