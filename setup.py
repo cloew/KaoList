cd2e0a947085d418764c810ee879b2daca0800a1
Chris Loew <cloew123@gmail.com>
2015-10-26 08:51:04 -0600

    Added proper dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 
 setup(name='kao_list',
-      version='0.0.1',
+      version='0.1.0',
       description="List Wrapper class that provides extra convenience methods such as first and last",
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       py_modules =['kao_list'],
+      install_requires = ['smart_defaults',
+                          'kao_decorators']
      )
